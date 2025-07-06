27e85ea6a83c3005ac15d947c4eb76e042cfa653
rpods <76547667+rpods@users.noreply.github.com>
2025-07-05 21:27:57 -0400

    Update yt2mp3.py
    Added comments to specify where to change values based on environment

--- yt2mp3.py
+++ yt2mp3.py
@@ -1,10 +1,10 @@
 import os
 import yt_dlp
 
-def youtube_to_mp3(youtube_url, output_path='.', cookies_file='www.youtube.com_cookies.txt'):
+def youtube_to_mp3(youtube_url, output_path='.', cookies_file='www.youtube.com_cookies.txt'): #Change cookies_file parameter here to point to the name of your youtube cookies.txt file
     ydl_opts = {
         'format': 'bestaudio/best',
-        'cookies': 'www.youtube.com_cookies.txt',
+        'cookies': 'www.youtube.com_cookies.txt', #Change cookies value here to point to the name of your youtube cookies.txt file
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
